3cef710b967e2f4a83ba9f6f519879b9c7f4b852
emmamjs-7 <emmaamjs@gmail.com>
2025-10-21 12:59:08 +0200

    footer with icons that navigates

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,13 +1,36 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
-  return <footer>
+  return <footer className='bg-primary fixed-bottom rounded-1 shadow-sm'>
     <Container fluid>
       <Row>
+        <Col className="text-center py-3">
+          <Link to="/" className="d-flex flex-column align-items-center text-decoration-none text-black"  >
+            <i className="bi bi-house-door-fill fs-1"></i>
+          </Link>
+        </Col>
+        <Col className="text-center py-3">
+          <Link to="/search" className="d-flex flex-column align-items-center text-decoration-none text-black">
+            <i className="bi bi-search fs-1"></i>
+          </Link>
+        </Col>
+        <Col className="text-center py-3">
+          <Link to="/create" className="d-flex flex-column align-items-center text-decoration-none text-black">
+            <i className="bi bi-plus-lg fs-1"></i>
+          </Link>
+        </Col>
+        <Col className="text-center py-3">
+          <Link to="/favourites" className="d-flex flex-column align-items-center text-decoration-none text-black">
+            <i className="bi bi-suit-heart-fill fs-1"></i>
+          </Link>
+        </Col>
         <Col className="text-center py-3 text-bg-primary">
-          © The Good Grocery {new Date().getFullYear()}
+          <Link to="/profile" className="d-flex flex-column align-items-center text-decoration-none text-black">
+            <i className="bi bi-person-fill fs-1"></i>
+          </Link>
         </Col>
       </Row>
     </Container>
-  </footer>;
+  </footer >;
 }
