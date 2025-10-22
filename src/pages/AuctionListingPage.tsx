21129b9f8c66431af45a0692ee8bf5b9e4ccac8e
Angelica <angelicanorlander@telia.com>
2025-10-22 15:01:17 +0200

    17- uses components and code from auctioncard to show bids and timeleft depending on sampleAuction

--- src/pages/AuctionListingPage.tsx
+++ src/pages/AuctionListingPage.tsx
@@ -1,13 +1,52 @@
 import { Row, Col } from "react-bootstrap";
 import Image from "../parts/Image";
 import { useState } from "react";
+import type { AuctionCard } from "../parts/AuctionCard";
 
 AboutPage.route = {
   path: "/listing",
 };
 
-export default function AboutPage(favorited: boolean) {
+//Imported things and copied things directly from auktioncard. This will be changed in future.
+
+const sampleAuction = {
+  id: 1,
+  title: "Scarf",
+  currentBid: 33,
+  endTime: new Date("2025-10-23T11:49:00"),
+  favorited: false,
+};
+
+export default function AboutPage({
+  id = sampleAuction.id,
+  title = sampleAuction.title,
+  currentBid = sampleAuction.currentBid,
+  endTime = sampleAuction.endTime,
+  favorited = sampleAuction.favorited,
+}: AuctionCard = sampleAuction) {
   const [isFavorited, setFavorited] = useState(favorited);
+  const currentTime = new Date();
+  let remainingTimeMessage = "";
+  // Time Difference in Milliseconds
+  const timeDifferenceMilli: number = endTime.getTime() - currentTime.getTime();
+
+  if (timeDifferenceMilli < 0) {
+    remainingTimeMessage = "Auction has finished";
+  } else {
+    const remainingSeconds = Math.floor(timeDifferenceMilli / 1000);
+    const remainingMinutes = Math.floor(remainingSeconds / 60);
+    const remainingHours = Math.floor(remainingMinutes / 60);
+    const remainingDays = Math.floor(remainingHours / 24);
+
+    // Only display the largest time unit remaining
+    if (remainingDays > 0) {
+      remainingTimeMessage = `${remainingDays} days`;
+    } else if (remainingHours > 0) {
+      remainingTimeMessage = `${remainingHours} hours`;
+    } else if (remainingMinutes > 0) {
+      remainingTimeMessage = `${remainingMinutes} minutes`;
+    } else remainingTimeMessage = `${remainingSeconds} seconds`;
+  }
 
   function onFavorite() {
     setFavorited((isFavorited) => !isFavorited);
@@ -16,10 +55,7 @@ export default function AboutPage(favorited: boolean) {
     <>
       <Row>
         <Col>
-          <Image
-            src="/images/products/3.jpg"
-            alt="A group photo of our employees."
-          />
+          <Image src="/images/products/3.jpg" alt="Here is a product" />
         </Col>
       </Row>
       <Row>
@@ -27,7 +63,7 @@ export default function AboutPage(favorited: boolean) {
           md={6}
           className="d-flex justify-content-between align-items-center"
         >
-          <h2 className="mb-0">Handmade vase</h2>
+          <h2 className="mb-0">{title}</h2>
           <span role="button" onClick={onFavorite} className="me-3">
             <i
               className={`bi bi-suit-heart${isFavorited ? "-fill" : ""} fs-1`}
@@ -35,6 +71,20 @@ export default function AboutPage(favorited: boolean) {
           </span>
         </Col>
       </Row>
+
+      <Row>
+        <Col md={6}>
+          {/*Current bid. start bid, timeleft.  */}
+          <p className="mb-1">Current bid: {currentBid} SEK</p>
+          <small className="text-muted">
+            Time left: {remainingTimeMessage}
+          </small>
+        </Col>
+      </Row>
+      {/* Add bid here */}
+      {/* Get Bidhistory here */}
+      {/* Get Item information here */}
+      {/* questions and comments here */}
     </>
   );
 }
