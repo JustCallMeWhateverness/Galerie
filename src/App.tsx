3ef5f29c5ef7e303783a8bbee40b480925a4ff03
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2025-10-30 12:06:48 +0100

    Cleaned up comment.

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,7 @@ export default function App() {
   return (
     <>
       <Header />
-      {/* <main className="site-main"> */}
       <Main />
-      {/* </main> */}
       <Footer />
     </>
   );
