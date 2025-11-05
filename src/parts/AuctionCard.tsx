f91760455820e28dfa59f390ecd57742330a6451
emmamjs-7 <emmaamjs@gmail.com>
2025-11-05 12:17:28 +0100

    AuctionCard now links to auction page

--- src/parts/AuctionCard.tsx
+++ src/parts/AuctionCard.tsx
@@ -2,24 +2,39 @@ import { Card } from 'react-bootstrap';
 import { useFavorite } from '../hooks/useFavorite';
 import { useAuth } from '../hooks/useAuth';
 import type Auction from '../interfaces/Auction';
+import { Link } from "react-router-dom";
 
 import AuthModal from '../modals/AuthModal';
 import { getRemainingTimeMessage } from '../utils/timeHelpers';
 
-export default function AuctionCard(props: Auction) {
+type Props = Auction & {
+  href?: string;
+};
 
-  const { id, title, currentBid, endTime } = props;
+export default function AuctionCard(props: Props) {
+
+  const { id, title, currentBid, endTime, href } = props;
   const { user } = useAuth();
   const isFavoritedByUser = !!user?.likedAuctions?.some(a => a.id === id);
   const { isFavorited, showAuthModal, onFavorite, setShowAuthModal } = useFavorite(isFavoritedByUser, props);
   const remainingTimeMessage = getRemainingTimeMessage(endTime);
+  const to = href ?? `/auction/${id}`;
+  const onFavClick: React.MouseEventHandler<HTMLSpanElement> = (e) => {
+    e.preventDefault();   // förhindra navigation
+    e.stopPropagation();  // stoppa bubbla
+    onFavorite();
+  };
 
   return (
     <>
-      <Card className="mb-4">
+      <Card
+        as={Link}
+        to={to}
+        className="mb-4"
+        style={{ cursor: "pointer" }}>
         <Card.Img style={{ minHeight: '200px', objectFit: 'cover' }} />
         <Card.ImgOverlay className='text-center'>
-          <span className='float-end' role='button' onClick={onFavorite}>
+          <span className='float-end' role='button' onClick={onFavClick}>
             {/* bi-suit-heart must be at the end for the correct logo to be shown */}
             <i className={`bi bi-suit-heart${isFavorited ? '-fill' : ''}`}></i>
           </span>
@@ -31,7 +46,7 @@ export default function AuctionCard(props: Auction) {
           </Card.Text>
         </Card.ImgOverlay>
         <Card.Body>
-          <Card.Text className='text-center'>
+          <Card.Text className='text-center text-decoration-none'>
             Current bid: {currentBid} SEK
           </Card.Text>
         </Card.Body>
